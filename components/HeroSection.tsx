2935399c7e7bec44373636a27ca52b7c9597321f
arao761 <basketballarao@gmail.com>
2024-08-22 21:53:15 -0400

    ads

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const images = [
-    '/logo.jpg',
+    '/improvedinnovatelogo.jpg',
     '/IMG_7961.jpeg',
     '/DSC_0339.jpeg',
 ];
