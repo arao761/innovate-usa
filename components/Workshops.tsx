a3e92698fa1e75da3736fd211703bb9ccdca6b76
arao761 <basketballarao@gmail.com>
2024-12-01 19:32:21 -0500

    ad

--- components/Workshops.tsx
+++ components/Workshops.tsx
@@ -124,7 +124,7 @@ const CompetitionsSection: React.FC = () => {
                 <div className="flex justify-between items-center mb-8">
                 <button 
                     onClick={handlePrev} 
-                    className="absolute left-4 top-1/2 transform -translate-y-1/2 text-yellow-400 hover:text-yellow-500 transition"
+                    className="absolute left-4 top-3/4 transform -translate-y-1/2 text-yellow-400 hover:text-yellow-500 transition"
                     aria-label="Previous"
                 >
                     <FaChevronLeft size={30} />
